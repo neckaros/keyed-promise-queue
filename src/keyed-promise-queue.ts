978044bc0a2e6c3ea12d4da76450921bce8fe2a4
Neckaros <arnaud@jezequel.org>
2019-03-16 15:12:21 +0100

    feat(keyed-promise-queue.ts): Now with possibility to have a max number of execution in parallel. Al
    BREAKING CHANGE: argument change from Promise to function that return a promise

--- src/keyed-promise-queue.ts
+++ src/keyed-promise-queue.ts
@@ -2,23 +2,70 @@
 // import "core-js/fn/array.find"
 // ...
 interface IResolver {
-  reject: () => void
-  resolve: () => void
+  reject: Reject
+  resolve: Resolve
+}
+type Resolve = (value?: any | PromiseLike<any>) => void
+type Reject = (reason?: any) => void
+
+interface IGlobalOptions {
+  timeout?: number
+  semaphore?: number
 }
 export class KeyedPromiseQueue {
-  public readonly keyedQueue = new Map<string, Promise<any>>()
+  public queueSize = 0
+  public readonly keyedQueue = new Map<string, { resolve: Resolve; reject: Reject }[]>()
+  public readonly pendingQueue: Array<{ key: string; process: () => PromiseLike<any> }> = []
 
-  processKeyed<T>(key: string, process: Promise<T>): Promise<T> {
+  constructor(public options: IGlobalOptions = {}) {}
+
+  processKeyed<T>(key: string, process: () => PromiseLike<T>): PromiseLike<T> {
     if (this.keyedQueue.has(key)) {
-      console.log('exist')
-      return this.keyedQueue.get(key)!
+      const resolvers = this.keyedQueue.get(key)!
+      return new Promise((resolve, reject) => resolvers.push({ resolve, reject }))
     }
-    const promessCleaner = process.then(p => {
-      this.keyedQueue.delete(key)
-      return p
-    })
-    this.keyedQueue.set(key, process)
+    const promess = new Promise<T>((resolve, reject) =>
+      this.keyedQueue.set(key, [{ resolve, reject }])
+    )
+    // if (options.timeout || this.options.timeout) {
+    //   this.timeout(options.timeout || this.options.timeout!);
+    // }
 
-    return promessCleaner
+    // const promessCleaner = process().then(p => {
+    //   console.log('cleaning')
+    //   this.keyedQueue.delete(key)
+    //   this.queueSize--;
+    //   this.take();
+    //   return p
+    // })
+    this.pendingQueue.push({ key, process })
+    this.take()
+    return promess
+  }
+
+  async take() {
+    // console.log('checking', this.pendingQueue.length, this.queueSize);
+    if (this.pendingQueue.length > 0) {
+      if (!this.options.semaphore || this.queueSize < this.options.semaphore) {
+        const el = this.pendingQueue.shift()!
+        this.queueSize++
+        if (this.options.timeout) {
+          await this.timeout(this.options.timeout)
+        }
+        el.process().then(r => {
+          const resolvers = this.keyedQueue.get(el.key)!
+          for (const resolver of resolvers) {
+            resolver.resolve(r)
+          }
+          this.keyedQueue.delete(el.key)
+          this.queueSize--
+          this.take()
+        })
+      }
+    }
+  }
+
+  timeout(ms: number) {
+    return new Promise(resolve => setTimeout(resolve, ms))
   }
 }
--- test/keyed-promise-queue.test.ts
+++ test/keyed-promise-queue.test.ts
@@ -12,34 +12,86 @@ class InheritedClass extends KeyedPromiseQueue {}
 describe('Class test', () => {
   it('Can instantiate a queue and add one task', async () => {
     const queue = new KeyedPromiseQueue()
-    const res = await queue.processKeyed('test', sleep(100, 'ok'))
+    const res = await queue.processKeyed('test', () => sleep(100, 'ok'))
     expect(res).toEqual('ok')
   })
+  it('Can instantiate with a global delay', async () => {
+    const queue = new KeyedPromiseQueue({ timeout: 50 })
+    const prom = queue.processKeyed('test', () => sleep(100, 'ok'))
+
+    const res = await prom
+
+    expect(res).toEqual('ok')
+  })
+
+  it('Can instantiate with a Semaphore', async () => {
+    const queue = new KeyedPromiseQueue({ semaphore: 1 })
+    const res = queue.processKeyed('test', () => sleep(400, 'ok'))
+    const res2 = queue.processKeyed('test2', () => sleep(400, 'Do'))
+    const res3 = queue.processKeyed('test3', () => sleep(400, '3'))
+    const [ret, ret2, ret3] = await Promise.all([res, res2, res3])
+    expect(ret).toEqual('ok')
+    expect(ret2).toEqual('Do')
+    expect(ret3).toEqual('3')
+  })
+
+  it('Can instantiate with a Semaphore 1', async () => {
+    const queue = new KeyedPromiseQueue({ semaphore: 1 })
+    let resolve1: (value?: {} | PromiseLike<{}>) => void
+    let resolve2: (value?: {} | PromiseLike<{}>) => void
+    const promess1 = () =>
+      new Promise(async (resolve, reject) => {
+        console.log('called1')
+        resolve1 = resolve
+        await sleep(100, 1)
+        expect(resolve2).toBeUndefined()
+        resolve(1)
+      })
+    const promess2 = () =>
+      new Promise(async resolve => {
+        console.log('called2')
+        resolve2 = resolve
+        expect(resolve1).toBeDefined()
+        await sleep(100, 2)
+        resolve(2)
+      })
+    const res = queue.processKeyed('test', promess1)
+    const res2 = queue.processKeyed('test2', promess2)
+    const [ret1, ret2] = await Promise.all([res, res2])
+    expect(ret1).toEqual(1)
+    expect(ret2).toEqual(2)
+  })
+
   it('Can instantiate a queue and add multiple task', async () => {
     const queue = new KeyedPromiseQueue()
-    const res1 = await queue.processKeyed('test', sleep(100, '1'))
-    const res2 = await queue.processKeyed('test1', sleep(150, 2))
-    const res3 = await queue.processKeyed('test2', sleep(100, '3'))
+    const res1 = await queue.processKeyed('test', () => sleep(100, '1'))
+    const res2 = await queue.processKeyed('test1', () => sleep(150, 2))
+    const res3 = await queue.processKeyed('test2', () => sleep(100, '3'))
     expect(res1).toStrictEqual('1')
     expect(res2).toStrictEqual(2)
     expect(res3).toStrictEqual('3')
   })
 
-  it('Same key should return initial promise', async () => {
+  it('Same key should return initial promise and not call second one', async () => {
     const queue = new KeyedPromiseQueue()
     const obj1 = { test: 1 }
     const obj2 = { test: 2 }
-    queue.processKeyed('test', sleep(100, obj1))
-    const res2 = await queue.processKeyed('test', sleep(100, obj2))
+    let called = false
+    queue.processKeyed('test', () => sleep(100, obj1))
+    const res2 = await queue.processKeyed('test', () => {
+      called = true
+      return sleep(100, obj2)
+    })
     expect(res2).toStrictEqual(obj1)
+    expect(called).toBeFalsy()
   })
 
   it('Same key but after initial resolution should return new promise', async () => {
     const queue = new KeyedPromiseQueue()
     const obj1 = { test: 1 }
     const obj2 = { test: 2 }
-    await queue.processKeyed('test', sleep(100, obj1))
-    const res2 = await queue.processKeyed('test', sleep(100, obj2))
+    await queue.processKeyed('test', () => sleep(100, obj1))
+    const res2 = await queue.processKeyed('test', () => sleep(100, obj2))
     expect(res2).toStrictEqual(obj2)
   })
 
@@ -47,8 +99,8 @@ describe('Class test', () => {
     const queue = new InheritedClass()
     const obj1 = { test: 1 }
     const obj2 = { test: 2 }
-    queue.processKeyed('test', sleep(100, obj1))
-    const res2 = await queue.processKeyed('test', sleep(100, obj2))
+    queue.processKeyed('test', () => sleep(100, obj1))
+    const res2 = await queue.processKeyed('test', () => sleep(100, obj2))
     expect(res2).toStrictEqual(obj1)
   })
 
